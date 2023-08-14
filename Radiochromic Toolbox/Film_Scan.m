e3dc862a3091e7308645a1202c76a20a99e98507
Ljubomir Kurij <ljubomir_kurij@protonmail.com>
2023-08-14 21:04:55 +0200

    [Project: Radiochromic Toolbox] Completed eleaborate doc string for the Film_Scan class.

--- Radiochromic Toolbox/Film_Scan.m
+++ Radiochromic Toolbox/Film_Scan.m
@@ -27,7 +27,7 @@ classdef Film_Scan
     %
     %       - film_type: 'EBT3'|'EBT4'|'EBT3P'|'EBT-XD'|'HD-V2'|'MD-V3'|...
     %             'RTQA2-1010'|'RTQA2-111'|'RTQA2-1417'|'XR-CT3'|'XR-RV3'|...
-    %             'XR-M3'|'LD-V1', def. 'EBT3'
+    %             'XR-M3'|'LD-V1'|'none', def. 'none'
     %             Type of the film used for the measurement. For the most up to
     %             date list of available film types, refer to Ashland's
     %             radiotherapy films website (https://www.ashland.com).
@@ -57,7 +57,7 @@ classdef Film_Scan
     %             to user defined value, or to the default value of 'none' if
     %             no value is given.
     %
-    %       - scan_resolution_unit: 'dpi'|'dpcm', def. 'none'
+    %       - scan_resolution_unit: 'dpi'|'dpcm'|'none', def. 'none'
     %             Unit of the scan resolution. Used to properly scale the scan
     %             to the correct size. The resolution unit is determined from
     %             the metadata of the tif file, if available. If no metadata is
@@ -92,11 +92,16 @@ classdef Film_Scan
     %
     %               - 'DeadPixels': A scan object with calculated dead pixels
     %                 for the given film piece. The dead pixels are calculated
-    %                 from the control scan.
+    %                 from the Film_Scan object of type 'Control' or
+    %                 'Irradiated' that is given as a parameter to the
+    %                 constructor.
     %
     %               - 'DummyDeadPixels': A scan object with calculated dead
     %                 pixels for the given dummy control film piece. The dead
-    %                 pixels are calculated from the dummy control scan.
+    %                 pixels are calculated from the Film_Scan object of type
+    %                 'DummyControl' or 'DummyIrradiated' that is given as a
+    %                 parameter to the constructor.
+    %
     %               - 'DummyControl': A dummy control scan, generated from the
     %                 given pixel values. The dummy control scan is used to
     %                 test various algorithms for analysis and pixel value
@@ -140,13 +145,13 @@ classdef Film_Scan
     %
     %
     %% Constructor parameters:
+    %
+    %       Positional parameters:
     %       - tif_file: string
     %             Absolute path to the tif file of the scan. The tif file must
     %             be a 16-bit color image (RGB). The pixel values are converted
     %             to double precision floating point numbers (double) before
-    %             further processing. If the tif_file is given, the
-    %             pixel_values matrix must not be given. Otherwise, an error is
-    %             reported.
+    %             further processing.
     %
     %       - pixel_values: double matrix
     %             Matrix of the pixel values of the scan. Used to support the
@@ -155,14 +160,26 @@ classdef Film_Scan
     %             the third dimension represents the RGB color channels. The
     %             pixel values must be in the range of 0 to 65535. The pixel
     %             values are converted to double precision floating point number
-    %             (double) before further processing. If the pixel_values
-    %             matrix is given, the tif_file must not be given. Otherwise,
-    %             an error is reported.
+    %             (double) before further processing.
     %
-    %       - Title: string, def. 'Film Scan'
-    %             Title of the scan (e.g. 'Film Scan 1'). See 'scan_title'
-    %             property for more information.
+    %       - film_scan: Film_Scan object
+    %             Film_Scan object of type 'Control', 'Irradiated',
+    %             'DummyControl', or 'DummyIrradiated'. The given Film_Scan
+    %             object is used to calculate the dead pixels of the scan. See
+    %             'scan_type' property for more information.
     %
+    %       Note: The 'Type' parameter is used to determine the type of the
+    %             object from which the Film_Scan object is created. The
+    %             'Control', 'Iradiated', and 'ZeroL' types require the
+    %             'tif_file' parameter as input. The 'DummyControl',
+    %             'DummyIrradiated', and 'DummyZeroL' types require the
+    %             'pixel_values' parameter as input. The 'DeadPixels' and
+    %             'DummyDeadPixels' types require the 'film_scan' parameter as
+    %             input. 'Unoknown' film scan can be created from any of the
+    %             above parameters. If the 'DeadPixels' or 'DummyDeadPixels'
+    %             types are used, the 'Smoothing' parameter is ignored.
+    %
+    %       Optional parameters:
     %       - DateOfIrradiation: serial date number (see: datenum), def. NaN
     %             Date of irradiation of the film. See 'date_of_irradiation'
     %             property for more information.
@@ -170,6 +187,33 @@ classdef Film_Scan
     %       - DateOfScan: serial date number (see: datenum), def. now
     %             Date of the scan of the film. See 'date_of_scan' property
     %             for more information.
+    %
+    %       - FilmLotNumber: string, def. 'none'
+    %             LOT number of the film used for the measurement. See
+    %             'film_lot_number' property for more information.
+    %
+    %       - FilmType: 'EBT3'|'EBT4'|'EBT3P'|'EBT-XD'|'HD-V2'|'MD-V3'|...
+    %             'RTQA2-1010'|'RTQA2-111'|'RTQA2-1417'|'XR-CT3'|'XR-RV3'|...
+    %             'XR-M3'|'LD-V1'|'none', def. 'none'
+    %             Type of the film used for the measurement. See 'film_type'
+    %             property for more information.
+    %
+    %       - Resolution: double, def. 400
+    %             Resolution of the scan in dots per inch (dpi). See
+    %             'scan_resolution' property for more information.
+    %
+    %       - ResolutionUnit: 'dpi'|'dpcm', def. 'dpi'
+    %             Unit of the scan resolution. See 'scan_resolution_unit'
+    %             property for more information.
+    %
+    %       - IrradiationType: 'Proton'|'Photon'|'Electron'|'Neutron'|...
+    %             'HeavyIon'|'Unknown', def. 'Unknown'
+    %             Type of the radiation used for the irradiation of the film.
+    %             See 'irradiation_type' property for more information.
+    %
+    %       - Title: string, def. 'Film Scan'
+    %             Title of the scan (e.g. 'Film Scan 1'). See 'scan_title'
+    %             property for more information.
     %
     %       - Type: 'Control'|'DeadPixels'|'DummyDeadPixels'|'DummyControl'|
     %             |'DummyIrradiated'|'DummyZeroL'|'Irradiated'|'ZeroL'|
@@ -184,17 +228,127 @@ classdef Film_Scan
     %             must be of the class Pixel_Value_Smoothing. For more
     %             information see the Pixel_Value_Smoothing class.
     %
-    %       - Resolution: double, def. 400
-    %             Resolution of the scan in dots per inch (dpi). See
-    %             'scan_resolution' property for more information.
-    %
-    %       - ResolutionUnit: 'dpi'|'dpcm', def. 'dpi'
-    %             Unit of the scan resolution. See 'scan_resolution_unit'
-    %             property for more information.
-    %
     %% Public methods:
     %
     %       - obj = Film_Scan(varargin): Class constructor.
+    %
+    %       - disp(obj): Display the Film_Scan object on the screen. The method
+    %             overrides the Octave's default 'disp' method.
+    %
+    %       -str_repr(obj): Returns a string representation of the Film_Scan
+    %             object.
+    %
+    %       - as_cell(obj): Returns a cell array representation of the Film_Scan
+    %             object.
+    %
+    %       - bool = eq(obj1, obj2): Returns true if the two Film_Scan objects
+    %             are equal, false otherwise. Two Film_Scan objects are equal
+    %             if all of their properties are equal. The method overrides
+    %             the Octave's default 'eq' method.
+    %
+    %       - bool = ne(obj1, obj2): Returns true if the two Film_Scan objects
+    %             are not equal, false otherwise. Two Film_Scan objects are not
+    %             equal if at least one of their properties is not equal. The
+    %             method overrides the Octave's default 'ne' method.
+    %
+    %       - bool = equi(obj1, obj2): Returns true if the two
+    %             Film_Scan objects are equivalent, false otherwise. Two
+    %             Film_Scan objects are equivalent if the following properties
+    %             are equal: 'date_of_irradiation', 'date_of_scan',
+    %             'film_lot_number', 'film_type', 'irradiation_type',
+    %             'scan_resolution', 'scan_resolution_unit', 'scan_type', and
+    %             pixel_values matrix size. The method is used to determine if
+    %             the two Film_Scan objects are compatible for analysis, i.e.
+    %             if the two Film_Scan objects can be used to generate a
+    %             Film_Scan_Set object.
+    %
+    %       - val = get(obj, prop): Returns the value of the given property of
+    %             the Film_Scan object. The method is used to access the
+    %             properties of the Film_Scan object. The method overrides the
+    %             Octave's default 'get' method. The following properties can
+    %             be accessed:
+    %               - 'date_of_irradiation'
+    %               - 'date_of_scan'
+    %               - 'film_lot_number'
+    %               - 'film_type'
+    %               - 'irradiation_type'
+    %               - 'pixel_values'
+    %               - 'scan_resolution'
+    %               - 'scan_resolution_unit'
+    %               - 'scan_title'
+    %               - 'scan_type'
+    %               - 'scan_file_name'
+    %               - 'scan_size'
+    %
+    %       - in_profile = in_profile(obj, x): Returns vector of pixel values
+    %             along the given column of the scan. The method is used to
+    %             extract the pixel values along the given column of the scan.
+    %             Parameter 'x' must be a positive integer in the range of 1 to
+    %             the width of the scan.
+    %
+    %       - cross_profile = in_profile(obj, y): Returns vector of pixel values
+    %             along the given row of the scan. The method is used to
+    %             extract the pixel values along the given row of the scan.
+    %             Parameter 'y' must be a positive integer in the range of 1 to
+    %             the height of the scan.
+    %
+    %       - hax = in_plot(obj, x): Plots the pixel values along the given
+    %             column of the scan and returns the handle to the axes object
+    %             of the plot. The method is used to plot the pixel values along
+    %             the given column of the scan. Parameter 'x' must be a positive
+    %             integer in the range of 1 to the width of the scan.
+    %
+    %       - hax = cross_plot(obj, y): Plots the pixel values along the given
+    %             row of the scan and returns the handle to the axes object of
+    %             the plot. The method is used to plot the pixel values along
+    %             the given row of the scan. Parameter 'y' must be a positive
+    %             integer in the range of 1 to the height of the scan.
+    %
+    %       - hax = imshow(obj): Plots the scan and returns the handle to the
+    %             axes object of the plot. The method is used to plot the scan
+    %             on the screen.
+    %
+    %
+    %% Static methods:
+    %
+    %       - obj = from_file(tif_file): Returns a Film_Scan object created from
+    %             the given tif file.
+    %
+    %       - obj = from_pixel_values(pixel_values): Returns a Film_Scan object
+    %             created from the given pixel values.
+    %
+    %       - obj = from_film_scan(film_scan): Returns a Film_Scan object
+    %             created from the given Film_Scan object.
+    %
+    %       - list_film_types(): Display the list of supported film types on the
+    %             screen.
+    %
+    %       - list_irradiation_types(): Display the list of supported
+    %             irradiation types on the screen.
+    %
+    %       - list_resolution_units(): Display the list of supported resolution
+    %             units on the screen.
+
+    %       - list_scan_types(): Display the list of supported scan types on
+    %             the screen.
+    %
+    %
+    %% Example:
+    %
+    %       % Create a Film_Scan object from a tif file
+    %       scan = Film_Scan.from_file('path/to/scan.tif');
+    %
+    %       % Create a Film_Scan object from pixel values
+    %       scan = Film_Scan.from_pixel_values(pixel_values);
+    %
+    %       % Create a Film_Scan object from a Film_Scan object
+    %       scan = Film_Scan.from_film_scan(film_scan);
+    %
+    %       % Display the list of supported film types
+    %       Film_Scan.list_film_types();
+    %
+    %       % Display the list of supported irradiation types
+    %       Film_Scan.list_irradiation_types();
     %
     %
     %% (C) Copyright 2023 Ljubomir Kurij
